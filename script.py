10b8cfee66b451097de55c476dfbf916557d4314
Soongil Cheoung <eunwho@gmail.com>
2014-12-16 22:39:51 +0900

    1. delete all mcp1 2. set to email jaejongmoon@gmail.com 3. set delevery time 3600sec 1 hour

--- script.py
+++ script.py
@@ -65,7 +65,6 @@ def send_email_2(txMessage):
 GPIO = webiopi.GPIO
 
 mcp0 = MCP23017(slave=0x20)
-mcp1 = MCP23017(slave=0x21)
 
 mcp0.setFunction(0, 1)
 mcp0.setFunction(1, 1)
@@ -85,24 +84,6 @@ mcp0.setFunction(13,0)
 mcp0.setFunction(14,0)
 mcp0.setFunction(15,0)
 
-mcp1.setFunction(0, 1)
-mcp1.setFunction(1, 1)
-mcp1.setFunction(2, 1)
-mcp1.setFunction(3, 1)
-mcp1.setFunction(4, 1)
-mcp1.setFunction(5, 1)
-mcp1.setFunction(6, 1)
-mcp1.setFunction(7, 1)
-
-mcp1.setFunction(8, 1)
-mcp1.setFunction(9, 1)
-mcp1.setFunction(10,1)
-mcp1.setFunction(11,0)
-mcp1.setFunction(12,0)
-mcp1.setFunction(13,0)
-mcp1.setFunction(14,0)
-mcp1.setFunction(15,0)
-
 mcp0.digitalWrite(0,1)
 mcp0.digitalWrite(1,0)
 mcp0.digitalWrite(2,1)
@@ -112,104 +93,6 @@ ptScale   = 1
 ptSecond  =380
 Ct        = 4
 
-
-E_DELAY = 0.0005
-E_PULSE = 0.0001
-
-class EW_LCD_23017(object):
-    #E_DELAY = 0.0005
-    #E_PULSE = 0.0001
-
-    def __init__(self):
-        mcp1.digitalWrite(9,0)  # RW
-        mcp1.digitalWrite(10,0)  # RW
-
-    def lcd_byte(self, data,mode):
-
-        if mode :
-            rs = 1
-        else:
-            rs = 0
-
-        mcp1.digitalWrite(8,rs)     # RS  
-
-        mcp1.digitalWrite(4,0)  
-        mcp1.digitalWrite(5,0)  
-        mcp1.digitalWrite(6,0)  
-        mcp1.digitalWrite(7,0)  
-
-        # high nibble setting
-        if data &0x10 == 0x10:
-            mcp1.digitalWrite(4,1)  
-        if data &0x20 == 0x20:
-            mcp1.digitalWrite(5,1)  
-        if data &0x40 == 0x40:
-            mcp1.digitalWrite(6,1)  
-        if data &0x80 == 0x80:
-            mcp1.digitalWrite(7,1)
-
-        # Toggle Enable Port
-        time.sleep(E_DELAY)  
-        mcp1.digitalWrite(9,1)      # CE
-        time.sleep(E_PULSE)  
-        mcp1.digitalWrite(9,0)
-        time.sleep(E_DELAY)  
-        
-        # low nibble setting
-        mcp1.digitalWrite(4,0)  
-        mcp1.digitalWrite(5,0)  
-        mcp1.digitalWrite(6,0)  
-        mcp1.digitalWrite(7,0)  
-
-        if data &0x01 == 0x01:
-            mcp1.digitalWrite(4,1)  
-        if data &0x02 == 0x02:
-            mcp1.digitalWrite(5,1)  
-        if data &0x04 == 0x04:
-            mcp1.digitalWrite(6,1)  
-        if data &0x08 == 0x08:
-            mcp1.digitalWrite(7,1)
-
-        # Toggle Enable Port
-        time.sleep(E_DELAY)  
-        mcp1.digitalWrite(9,1)
-        time.sleep(E_PULSE)  
-        mcp1.digitalWrite(9,0)
-        time.sleep(E_DELAY)  
-
-
-class HD47780(object):
-    LCD_CHR = 1
-    LCD_CMD = 0
-    # Base addresses for lines on a 20x4 display
-    LCD_BASE = 0x80, 0xC0, 0x94, 0xD4
-    #LCD_BASE = 0x00, 0x40, 0x20, 0x54
-
-    def __init__(self, driver, rows=2, width=16):
-        self.rows = rows
-        self.width = width
-        self.driver = driver
-        self.lcd_init()
-
-    def lcd_init(self):
-        # Initialise display
-        lcd_byte = self.driver.lcd_byte
-        for i in 0x33, 0x32, 0x28, 0x0C, 0x06, 0x01:
-        #for i in 0x01, 0x06, 0x80, 0x01:
-            lcd_byte( i,self.LCD_CMD)
-
-    def lcd_string(self, message, line):
-        # Send string to display
-        lcd_byte = self.driver.lcd_byte
-        lcd_byte(self.LCD_BASE[line], self.LCD_CMD)
-        #for i in bytearray(message.ljust(self.width)):
-        for i in bytearray(message.encode()):
-            lcd_byte(i,self.LCD_CHR)
-
-
-driver1 = EW_LCD_23017( )
-lcd1 = HD47780(driver=driver1, rows=2, width=16)
-lcd1.lcd_string('Eun Who P.E.',1)
 
 # Enable debug output
 webiopi.setDebug()
@@ -335,33 +218,20 @@ def loop():
     pilotLed = not mcp0.digitalRead(7)
     mcp0.digitalWrite(7,pilotLed)
     
-    #driver1 = EW_LCD_23017( )
-    #lcd1 = HD47780(driver=driver1, rows=2, width=16)
-
-    if not mcp1.digitalRead(11):
-        #get_ip_address_2():
-        lcd1.lcd_string(get_ip_address_2(), 0 )
-    
-    if elapsedTime > 30 : 
+    if elapsedTime > 3600 : 
 
         if not mcp0.digitalRead(8):
-            lcd1.lcd_string(" DIN 1 ON  ", 1 )
             send_email_2(' ALARM DIN 1 ON ') 
             startTime = time.time()
         elif not mcp0.digitalRead(9):
-            lcd1.lcd_string(" DIN 2 ON   ", 1 )
             send_email_2(' ALARM DIN 2 ON ') 
             startTime = time.time()
         elif not mcp0.digitalRead(10):
-            lcd1.lcd_string(" DIN 3 ON  ", 1 )
             send_email_2(' ALARM DIN 3 ON ') 
             startTime = time.time()
         elif not mcp0.digitalRead(11):
-            lcd1.lcd_string(" DIN 4 ON  ", 1 )
             send_email_2('  ALARM DIN 4 ON ') 
             startTime = time.time()
-        else:
-            lcd1.lcd_string("DIN ALL OFF", 1 )
 
     webiopi.debug("loop message")
     webiopi.sleep(5)
